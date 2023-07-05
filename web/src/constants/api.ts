40f128a33cd4ead8dcf09ac5f8282a7e535fd6ab
Govind Singh <govind.s@enlume.com>
2023-07-05 09:54:55 +0530

    update baseurl

--- web/src/constants/api.ts
+++ web/src/constants/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8080/api', // Replace with your backend URL
+    baseURL: 'https://sunoclear.onrender.com/api', // Replace with your backend URL
     headers: {
       'Content-Type': 'application/json',
       // Add any other headers you need, such as authorization headers
